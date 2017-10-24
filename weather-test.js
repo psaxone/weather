38de2f80252377746ddf671af6d8e6646888390a
Ariel Sansone <32747901+psaxone@users.noreply.github.com>
2017-10-24 20:15:21 -0300

    casi casi

--- weather-test.js
+++ weather-test.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $("button").click(function(){
-        var apiKEY = "&appid=af95bc4e30710dff7080cfb67eadba30"
+        var apiKEY = "&units=metric&appid=af95bc4e30710dff7080cfb67eadba30"
         var apiURL = 'http://api.openweathermap.org/data/2.5/weather?q='
         var ciudad = $("#ciudad").val();
         var URLcompleta = apiURL+ciudad+apiKEY
@@ -8,7 +8,9 @@ $(document).ready(function(){
         $.get(URLcompleta, function( data ) {
             console.log( data );
             $(document).ready(function(){
-                $("p").append(data.name);
+                $("#city").append(data.name);
+                $("#temperature").append(data.main.temp);
+                $("#pressure").append(data.main.pressure);
             });   
         });
     });
